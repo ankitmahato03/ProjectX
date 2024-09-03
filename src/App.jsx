10f0cc422fc150e76bc70eb3575113d4cf3d3db7
Jeesun Bari <jeesunbari2002@gmail.com>
2024-09-04 00:45:26 +0530

    Implement lazy loading for components and update navigation links

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import Hero from "./components/Hero";
 import Navbar from "./components/Navbar";
-import { lazy } from "react";
+import { lazy, Suspense } from "react";
 const OurTeam = lazy(() => import('./components/OurTeam'));
 const OurExpertise = lazy(() => import('./components/OurExpertise'));
 const OurProject = lazy(() => import('./components/OurProject'));
@@ -15,13 +15,27 @@ const App = () => {
     <>
       <Navbar />
       <Hero />
-      <OurExpertise />
-      <OurProject />
-      <ClientReview />
-      <Workflow />
-      <OurTeam />
-      <Contact />
-      <Footer />
+      <Suspense fallback={<div>Loading...</div>}>
+        <OurExpertise />
+      </Suspense>
+      <Suspense fallback={<div>Loading...</div>}>
+        <OurProject />
+      </Suspense>
+      <Suspense fallback={<div>Loading...</div>}>
+        <ClientReview />
+      </Suspense>
+      <Suspense fallback={<div className="">Loading...</div>}>
+        <Workflow />
+      </Suspense>
+      <Suspense fallback={<div>Loading...</div>}>
+        <OurTeam />
+      </Suspense>
+      <Suspense fallback={<div>Loading...</div>}>
+        <Contact />
+      </Suspense>
+      <Suspense fallback={<div>Loading...</div>}>
+        <Footer />
+      </Suspense>
     </>
   );
 };
